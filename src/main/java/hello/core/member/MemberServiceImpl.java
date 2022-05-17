ccdbf0576c19fa0b3ae80f379fa8a29ca107e02b
Jisu Choi <gjkl55@naver.com>
2022-05-17 22:56:44 +0900

    [추가] 관심사 분리, 생성자 주입, 추상화에만 의존 - Spring X

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    //추상화에만 의존
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    //생성자 주입
 
     @Override
     public void join(Member member) {
