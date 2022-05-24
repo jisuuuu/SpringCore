034fbd861f85f8aff8f768d2e3e41a21006a6851
Jisu Choi <gjkl55@naver.com>
2022-05-24 22:04:54 +0900

    [추가 & 수정] 싱글톤 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -26,6 +28,22 @@ public class SingletonTest {
         //jvm 메모리에 계속 생성되어 만들어짐 => 메모리 낭비 발생
 
         //memberService != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //same: 진짜 똑같은지 ==
+
+        singletonService1.logic();
+        //스프링은 기본적으로 싱글톤임
     }
 }
