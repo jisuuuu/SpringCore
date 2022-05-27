9b8585437bf3c2285ccc8dfa461f75a7ff8a9d0f
Jisu Choi <gjkl55@naver.com>
2022-05-27 23:16:51 +0900

    [추가] 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,23 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", //어디서 부터 찾을지 지정 가능, 지정 안하면 전부 다 스캔
+        //basePackages = "hello.core.member", //어디서 부터 찾을지 지정 가능, 지정 안하면 전부 다 스캔
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //AppConfig와 충돌 되지 않기 위해
 )
 public class AutoAppConfig {
+
+    //충돌 안남 -> 수동으로 등록한 것이 우선 권 -> 충돌은 안나는데 override
+    @Bean("memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
