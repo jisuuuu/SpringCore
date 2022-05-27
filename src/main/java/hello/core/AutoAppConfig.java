30d4835240c007b767c04b00aaa406e78f15d11b
Jisu Choi <gjkl55@naver.com>
2022-05-27 23:05:10 +0900

    [추가] 필터 기능

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member", //어디서 부터 찾을지 지정 가능, 지정 안하면 전부 다 스캔
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //AppConfig와 충돌 되지 않기 위해
 )
