b071efd94028ff47a84eb6b17974c6f2f83f8100
Jisu Choi <gjkl55@naver.com>
2022-05-28 20:19:19 +0900

    [수정] 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,15 +9,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+    //필드에 바로 주입하는 것 -> 간단하지만 권장하지 X -> 외부에서 변경 X
     //인터페이스에만 의존
 
+    //수정자 주입 -> 변경 가능성 존재
     @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    /*
+    //@Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    //생성자가 딱 하나일 때는 @Autowired 생략되어도 @Component만 있어도 자동으로 주입 됨
+     */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
