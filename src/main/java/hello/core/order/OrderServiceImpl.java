34a627b090a8017f5a0893f0d9d8f1657180917f
Jisu Choi <gjkl55@naver.com>
2022-05-30 22:12:48 +0900

    [수정&추가] 생성자 주입 예시, 테스트 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,10 +9,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
     //필드에 바로 주입하는 것 -> 간단하지만 권장하지 X -> 외부에서 변경 X
     //인터페이스에만 의존
+    //생성자 주입을 권장하는 이유 -> final 사용할 수 있음 다른 주입 방법은 사용 불가
 
     //수정자 주입 -> 변경 가능성 존재
     /*@Autowired
@@ -27,19 +28,18 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
     }*/
 
-
-    //@Autowired
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
     //생성자가 딱 하나일 때는 @Autowired 생략되어도 @Component만 있어도 자동으로 주입 됨
 
-    @Autowired
+    /*@Autowired
     public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    }*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
